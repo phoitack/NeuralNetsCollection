07f589c2d4d0f5e6c694a9a94ede3f506eff4c0c
Barak28 <36894021+Barak28@users.noreply.github.com>
2019-12-28 10:50:34 +0200

    Add files via upload

--- net1.py
+++ net1.py
@@ -10,7 +10,57 @@ plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.show()
 print(X.shape, y.shape)
 
+# Net Parameters
 input_neurons = 2
 output_neurons = 2
 samples = X.shape[0]
-learning_rate = 0.01
+learning_rate = 0.001
+lambda_reg = 0.01
+
+# Dictionary definition
+
+model_dic = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+def retreive(model_dict):
+    W1 = model_dic['W1']
+    b1 = model_dic['b1']
+    W2 = model_dic['W2']
+    b2 = model_dic['b2']
+    return W1, b1, W2, b2
+
+# Forward propagation
+def forward(x, model_dict):
+    W1, b1, W2, b2 = retreive(model_dict)
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    exp_scores = np.exp(a2)
+    softmax = exp_scores / np.sum(exp_scores, dim=1, keepdims=True)  #  dim =1 is the columns
+    return softmax
+
+# Loss definition
+def loss(softmax, y):
+    W1, b1, W2, b2 = retreive(model_dict)
+    m = np.zeros(200)
+    for i, correct_index in enumerate(y):
+        predicted = softmax[i][correct_index]
+        m[i] = predicted
+    log_prob = -np.log(predicted)
+    loss = np.sum(log_prob)
+    reg_loss = lambda_reg / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    loss += reg_loss
+    return float(loss / y.shape[0])
+
+def predict(x, model_dict):
+    W1, b1, W2, b2 = retreive(model_dict)
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    exp_scores = np.exp(a2)
+    softmax = exp_scores / np.sum(exp_scores, dim=1, keepdims=True)
+    return np.argmax(softmax, axis=1)
+
+# Backpropagation
+
+
